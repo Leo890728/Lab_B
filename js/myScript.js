b8e8d3b28592f22c7e09f30e584d2e501effed0f
Leo890728 <s123469410@gmail.com>
2024-03-01 14:46:06 +0800

    新增彈出div

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,15 @@
+
+var modalElem = document.getElementById("tyModal");
+var spanElem = document.getElementsByClassName("tyClose")[0];
+
+spanElem.onclick = function () {
+    modalElem.style.display = "none";
+}
+
+setTimeout(function () {
+    modalElem.style.display = "block";
+}, 1000);
+
 // Tabs
 function openLink(evt, linkName) {
     var i, x, tablinks;
